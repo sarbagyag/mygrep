1e22fac38008342cfc267925385feb3befcdc771
sarbagya <sarbagyaghoshrestha@gmail.com>
2026-04-26 19:40:48 +0545

    Project 1: grep clone complete

--- main.c
+++ main.c
@@ -1,54 +1,44 @@
+#include "grep.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "grep.h"
 
-void usage(const char *program_name){
-  fprintf(stderr, "Usage: %s [-n] [-i] [-c] pattern [file...]\n",program_name);
+void usage(const char *program_name) {
+  fprintf(stderr, "Usage: %s [-n] [-i] [-c] pattern [file...]\n", program_name);
   exit(1);
 }
 
-
-int main(int argc, char *argv[]){
-  if(argc<2){
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
     usage(argv[0]);
   }
 
-  GrepOptions opts={0};
-  int i=1;
+  GrepOptions opts = {0};
+  int i = 1;
 
-  while(i<argc && argv[i][0]=='-'){
-    if (strcmp(argv[i], "-n")==0){
-      opts.line_numbers=1;
-    }else if (strcmp(argv[i], "-i")==0){
-       opts.ignore_cases=1;
-    }else if (strcmp(argv[i], "-c")==0){
-      opts.count_only=1;
-    }else{
+  while (i < argc && argv[i][0] == '-') {
+    if (strcmp(argv[i], "-n") == 0) {
+      opts.line_numbers = 1;
+    } else if (strcmp(argv[i], "-i") == 0) {
+      opts.ignore_cases = 1;
+    } else if (strcmp(argv[i], "-c") == 0) {
+      opts.count_only = 1;
+    } else {
       fprintf(stderr, "Unknown flag: %s\n", argv[i]);
       usage(argv[0]);
     }
     i++;
   }
 
-  if(i>=argc){
+  if (i >= argc) {
     fprintf(stderr, "Error: no pattern given\n");
     usage(argv[0]);
-
   }
 
-  opts.pattern=argv[i];
-  int file_start=i+1;
+  opts.pattern = argv[i];
+  int file_start = i + 1;
 
   process_files(argc, argv, file_start, &opts);
 
   return 0;
-
 }
-
-
-
-
-
-
-
